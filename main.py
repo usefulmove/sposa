e87b646bd6e4855cb4e119e75ebbc8a3370f6880
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-07 20:45:48 -0800

    base implementation of concept

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
+#!/usr/bin/env python
 import sys
+from time import sleep
+# from rich import Console
 
 
 def sposa():
-    file: str = sys.argv[1]
+    filename: str = sys.argv[1]
 
-    print(f"{file=}")
-
-    with open(file, 'r') as file:
+    with open(filename, "r") as file:
         content = file.read()
 
     words = content.split()
 
-    for word in words:
-        print(word)
+    # hide cursor
+    sys.stdout.write("\033[?25l")
+    sys.stdout.flush()
 
-    print("hello sposa.")
+    try:
+        sys.stdout.write("\r\033[2K" + f"  {words[0]}  ")
+        sys.stdout.flush()
+
+        sleep(1.0)
+
+
+        for word in words:
+            # clear line properly
+            sys.stdout.write("\r\033[2K" + f"  {word}  ")
+            sys.stdout.flush()
+
+            sleep(0.318)
+
+            # increase delay for punctuation
+            if word and word[-1] in ".,:!?;":
+                sleep(0.280)
+
+    finally:
+        # show cursor again
+        sys.stdout.write("\033[?25h")
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+
+    sleep(1.0)
 
 
 if __name__ == "__main__":
